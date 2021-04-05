3014d8e762dffb3fec558cb6d717293dac659074
rafifauz <rafitugas3@gmail.com>
2021-04-05 16:44:11 +0700

    v7 Rearange

--- MERN-app/Production/run-k8s.sh
+++ MERN-app/Production/run-k8s.sh
@@ -1,5 +1,6 @@
-kubectl create -f mongodb-pod.yml
+kubectl create namespace production
 kubectl create -f mongodb-service.yml
+kubectl create -f mongodb-pod.yml
 kubectl create -f backend-service.yml
 kubectl create -f backend-deployment.yml
 kubectl create -f front-end-service.yml
